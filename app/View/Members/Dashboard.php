8af223cbcdef0e69d47006c7da571f857ea69593
Sanchit <dev@minutephp.com>
2016-11-10 13:59:20 +0530

    automatic

--- app/View/Members/Dashboard.php
+++
@@ -1,9 +0,0 @@
-<div class="content-wrapper ng-cloak" ng-app="DashboardApp" ng-controller="DashboardController as mainCtrl" ng-init="init()" ng-cloak="">
-
-<section class="content debug">
-<!-- var dump start -->
-hi
-<!-- var dump end -->
-</section>
-
-</div>
